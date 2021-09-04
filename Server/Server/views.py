2ab98eb11ed0f4bf4a3336f6d4c60847fcc6769d
Minshen Lu <lums19960506@gmail.com>
2021-09-04 21:39:09 +0800

    部署 django

--- Server/Server/views.py
+++ Server/Server/views.py
@@ -1,6 +1,6 @@
 import json
 from django.http import HttpResponse
-
+import os
 
 def entrance(request):
     print("Get Request～")
@@ -16,6 +16,13 @@ def entrance(request):
     while True:
         if os.path.exists(log_path):
             print("succeed to get log!")
+            os.remove(imgData)
+            with open(log_path, "r") as f2:
+                res = json.load(f2)
+            os.remove(log_path)
+            for light in res["res"]:
+                if light['label'] == "1":
+                    return HttpResponse({"msg": "failed"})
             break
         time.sleep(0.1)
         heartbeat += 1
