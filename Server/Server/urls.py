b20c04fd380e5335caee3a83ddeb39cdabf85070
Minshen Lu <lums19960506@gmail.com>
2021-09-04 15:10:36 +0800

    部署 django

--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
-from . import views
+from Server.views import entrance
+
 
 urlpatterns = [
-    path('admin/', views.entrance),
+    path(r'test/', entrance),
 ]
--- Server/Server/views.py
+++ Server/Server/views.py
@@ -1,10 +1,9 @@
 import json
-
 from django.http import HttpResponse
 
 
-def entrance(result, request):
-    # print("收到了请求～")
-    # body = str(request.body, encoding="utf-8")
-    # print("Body:", body)
-    return HttpResponse(json.dumps({"msg": "success"}))
+def entrance(request):
+    print("收到了请求～")
+    body = str(request.body, encoding="utf-8")
+    print("Body:", body)
+    return HttpResponse({"msg": "success"})
