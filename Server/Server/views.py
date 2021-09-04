5177c398520f2f20762e9a0ac8d642ee12e6567f
Minshen Lu <lums19960506@gmail.com>
2021-09-04 14:48:16 +0800

    部署 django

--- Server/Server/views.py
+++ Server/Server/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def entrance(result, request):
-    print("收到了请求～")
-    body = str(request.body, encoding="utf-8")
-    print("Body:", body)
+    # print("收到了请求～")
+    # body = str(request.body, encoding="utf-8")
+    # print("Body:", body)
     return HttpResponse(json.dumps({"msg": "success"}))
